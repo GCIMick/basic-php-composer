ab981c9fbd801b47a9d0989cbdbbeb619a323200
GCIMick <michael.gallagher@gcisolutions.com.au>
2024-07-26 14:55:24 +1000

    sqltest

---
+++ sqltest.php
@@ -0,0 +1,13 @@
+<?php
+// Using an access token to connect: do not use UID or PWD connection options
+// Assume $accToken is the valid byte string extracted from an OAuth JSON response
+$connectionInfo = array("Database"=>$azureAdDatabase, "AccessToken"=>$accToken);
+$conn = sqlsrv_connect($azureAdServer, $connectionInfo);
+if ($conn === false) {
+    echo "Could not connect with Azure AD Access Token.\n";
+    print_r(sqlsrv_errors());
+} else {
+    echo "Connected successfully with Azure AD Access Token.\n";
+    sqlsrv_close($conn);
+}
+?>
